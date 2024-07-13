a6fb26271d26f2a72420ee593ce9215ab026d35c
Alexandre <alepateis9090@gmail.com>
2024-07-13 11:49:50 -0300

    adicionando devise e atuticação

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :appointments
   get 'reports', to: 'reports#index'
   resources :clients
@@ -6,5 +7,9 @@ Rails.application.routes.draw do
   root 'dashboard#index'
   resources :services
   get "up" => "rails/health#show", as: :rails_health_check
+
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
 
 end
